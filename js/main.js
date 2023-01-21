3c1d39fd3b2d47338c69d3d46a94075b9daa99e0
Елена <120094132+ElenaGribkova@users.noreply.github.com>
2023-01-21 09:25:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const prev = document.getElementById('btn-prev'),
+    next = document.getElementById('btn-next'),
+    slides = document.querySelectorAll('.slide'),
+    dots = document.querySelectorAll('.dot');
+    titles = document.querySelectorAll('.slider__titles');
+
+
+let index = 0;
+
+const activSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+const activDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active'); 
+}
+const activTit = n => {
+    for (tit of titles) {
+        tit.classList.remove('active');
+    }
+    titles[n].classList.add('active'); 
+}
+
+const prepareCurentSlide = ind => {
+    activSlide(ind);
+    activDot(ind);
+    activTit(ind);
+}
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        prepareCurentSlide(index);
+    } else {
+        index++;
+        prepareCurentSlide(index);
+    }
+}
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1
+        prepareCurentSlide(index);
+    } else {
+        index--;
+        prepareCurentSlide(index);
+    }
+}
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click',()=>{
+    index = indexDot;
+    prepareCurentSlide(index);
+})
+})
+
+titles.forEach((item, indexTit) => {
+    item.addEventListener('click',()=>{
+    index = indexTit;
+    prepareCurentSlide(index);
+})
+})
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
